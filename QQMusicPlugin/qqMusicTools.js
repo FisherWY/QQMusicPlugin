b5f9d7c4cebdcb7c77bbfa3c09d6a92ad75177e7
Fisher <32707008+FisherWY@users.noreply.github.com>
2019-05-01 22:31:37 +0800

    添加了2个API
    1. 添加了获取TOP100歌单API
    2. 添加了随机推荐歌曲API

--- QQMusicPlugin/qqMusicTools.js
+++ QQMusicPlugin/qqMusicTools.js
@@ -62,6 +62,7 @@ function getMusicKey(filename) {
 function getMusicSrc(filename, token) {
   return new Promise((resolve, reject) => {
     var reqUrl = "http://ws.stream.qqmusic.qq.com/C400" + filename + ".m4a?guid=126548448&vkey=" + token + "&fromtag=0"
+    console.log("at get music src: 音乐播放链接生成成功")
     resolve(reqUrl)
   })
 }
@@ -71,6 +72,7 @@ function playMusic(filename) {
   return new Promise((resolve, reject) => {
     getMusicKey(filename).then(function (res) {
       getMusicSrc(filename, res.data.data.items[0].vkey).then(function (res) {
+      	console.log("at play music: 返回音乐播放链接")
         resolve(res)
       })
     })
@@ -82,13 +84,58 @@ function getAlbumImage(albumId) {
   return new Promise((resolve, reject) => {
     var id = parseInt(albumId)
     var imgUrl = 'http://imgcache.qq.com/music/photo/album_300/'+id%100+'/300_albumpic_'+id+'_0.jpg'
+    console.log("at get album image: 返回音乐专辑图片链接")
     resolve(imgUrl)
   })
+}
+
+// QQ音乐获取TOP100歌单
+function getTopMusic() {
+	return new Promise((resolve, reject) => {
+		var getUrl = "https://c.y.qq.com/v8/fcg-bin/fcg_v8_toplist_cp.fcg?g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8¬ice=0&platform=h5&needNewCode=1&tpl=3&page=detail&type=top&topid=27&_=1519963122923"
+		wx.request({
+			url: getUrl,
+			method: 'get',
+			success: function(res) {
+				console.log("at get top music: 获取音乐Top100歌单成功")
+				// console.log(res)
+				resolve(res)
+			},
+			fail: function(res) {
+				console.log("at get top music: 获取音乐Top100歌单失败")
+				// console.log(res)
+				reject(res)
+			}
+		})
+	})
+}
+
+// QQ音乐随机推荐
+function randomRecommend() {
+	return new Promise((resolve, reject) => {
+		var getUrl = "https://c.y.qq.com/v8/fcg-bin/fcg_v8_toplist_cp.fcg?g_tk=5381&uin=0&format=json&inCharset=utf-8&outCharset=utf-8¬ice=0&platform=h5&needNewCode=1&tpl=3&page=detail&type=top&topid=36&_=1520777874472"
+		wx.request({
+			url: getUrl,
+			method: 'GET',
+			success: function(res) {
+				console.log("at random recommend: 获取随机推荐歌曲成功")
+				// console.log(res)
+				resolve(res)
+			},
+			fail: function(res) {
+				console.log("at random recommend: 获取随机推荐歌曲失败")
+				// console.log(res)
+				reject(res)
+			}
+		})
+	})
 }
 
 module.exports = {
   Promise,
   searchMusic: searchMusic,
   playMusic: playMusic,
-  getAlbumImage: getAlbumImage
+  getAlbumImage: getAlbumImage,
+  getTopMusic: getTopMusic,
+  randomRecommend: randomRecommend
 }
